69f63fb9922061c869f1666a78176654cd1384a8
nnagewad <nikin.nagewadia@icloud.com>
2024-01-30 15:30:31 +0000

    Pat implemented data base and autocomplete

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,5 +6,23 @@
 window.GOVUKPrototypeKit.documentReady(() => {
   // Add JavaScript here
 
+  console.log(document.querySelector("#assignee"))
 
+  fetch("/public/test.json").then(data => data.json()).then(data => {
+    let firstRow = document.querySelectorAll(".govuk-table__row")[1];
+
+    data.forEach(({ title, assignee, state }) => {
+      let newRow = firstRow.cloneNode(true);
+
+      newRow.querySelector('.title').innerHTML = `<a href="#">${title}</a>`
+      newRow.querySelector('.assignee').innerText = assignee
+      newRow.querySelector('.state').innerHTML = `<strong class="govuk-tag">${state}</strong>`
+
+      firstRow.parentNode.append(newRow)
+    });
+  });
+
+  accessibleAutocomplete.enhanceSelectElement({
+    selectElement: document.querySelector("#assignee")
+  });  
 })
