d91ebf103c5b183eda1f8c75e8fef08ebbe183dd
Patrick Cartlidge <patrickcartlidge@gmail.com>
2024-02-01 12:33:59 +0000

    filter stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,8 +4,10 @@
 //
 
 window.GOVUKPrototypeKit.documentReady(() => {
-  const matchParam = (param, input) =>
-    input && (input.replace(' ','').toLowerCase()).match(param.replace(' ','').toLowerCase())
+  const matchParam = (param, input, exact) =>
+    input && ((input.replace(' ','').toLowerCase()).match(
+      !exact ? param.replace(' ','').toLowerCase() : new RegExp(`^${param.replace(' ','').toLowerCase()}$`)
+    ))
 
   fetch("/public/test.json").then(data => data.json()).then(data => {
     let firstRow = document.querySelectorAll(".govuk-table__row")[1];
@@ -32,13 +34,21 @@ window.GOVUKPrototypeKit.documentReady(() => {
         'title',
         'assignee',
         'format',
+        'state'
       ]
 
-      let params = Array.from(new URL(document.location).searchParams.entries()).filter(param => whitelist.includes(param[0]) && param[1].length);
+      let matchExact = {
+        title: false,
+        assignee: true,
+        format: true,
+        state: true,
+      }
+
+      let params = Array.from(new URL(document.location).searchParams.entries()).filter(param => whitelist.includes(param[0]) && param[1].length && param[1]!='_unchecked');
 
       // ...i guess it's an AND filter...
       if (params.length) {
-        if (params.filter(param => matchParam(param[1], data[index][param[0]])).length == params.length) {
+        if (params.filter(param => matchParam(param[1], data[index][param[0]], matchExact[param[0]])).length == params.length) {
           firstRow.parentNode.append(newRow)  
         }
       } else {
