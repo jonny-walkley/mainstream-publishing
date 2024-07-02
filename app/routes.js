d662a843194ff9ee5bde487b07c552e1090bd924
Rebecca Cottrell <r.e.cottrell@gmail.com>
2024-07-02 13:48:59 +0100

    Add links between things

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,15 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+
+router.post('/country-answer', function(request, response) {
+
+    var breadcrumboptions = request.session.data['breadcrumboptions']
+    if (breadcrumb-options == "choose breadcrumb"){
+        response.redirect("/browse-breadcrumb-options")
+    } else {
+        response.redirect("/breadcrumb-confirmation")
+    }
+})
+
