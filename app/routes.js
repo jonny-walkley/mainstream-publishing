aeb24ff114e7a11d9d0f7cf23def59e66c40b073
jonny-walkley <jonny.walkley@digital.cabinet-office.gov.uk>
2025-02-25 17:57:50 +0000

    Add UR 2ier flow

--- app/routes.js
+++ app/routes.js
@@ -8,6 +8,31 @@ const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
 
+// Log previous page, current page and session data
+
+// router.use('/', (req, res, next) => {
+//     const lastItem = req.get('Referrer').substring(req.get('Referrer').lastIndexOf('/') + 1);
+//     const log = {
+//         previous_page: "/" + lastItem,
+//         current_page: req.url,
+//         data: req.session.data
+//     }
+//     console.log("========== START ==========");
+//     console.log(JSON.stringify(log, null, 2));
+//     console.log("========== END ==========");
+//     console.log("                           ");
+//     console.log("                           ");
+//     console.log("                           ");
+//     next();
+// });
+
+router.use((req, res, next) => {
+    const log = {
+        data: req.session.data
+    }
+    console.log(JSON.stringify(log, null, 2));
+    next();
+})
 
 router.post('/ur-1-pension-credit-breadcrumb-answer', function(request, response) {
 
@@ -18,4 +43,3 @@ router.post('/ur-1-pension-credit-breadcrumb-answer', function(request, response
         response.redirect("/ur-1-breadcrumb-selection")
     }
 })
-
