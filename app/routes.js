78d4e4f5fd0a7cae09de3f1587d5aa5d9be52c97
Jonny Walkley <JWalkley@MAC-Dev-L3PC44MR4Y.local>
2026-09-16 23:22:01 +0100

    Add support app create user

--- app/routes.js
+++ app/routes.js
@@ -42,4 +42,77 @@ router.post('/ur-1-pension-credit-breadcrumb-answer', function(request, response
     } else {
         response.redirect("/ur-1-breadcrumb-selection")
     }
+
+})
+
+// Support app routes
+
+router.post('/support-app/who-is-this-request-for', function (req, res) {
+  res.render('support-app/what-are-you-requesting')
+})
+
+router.post('/support-app/what-are-you-requesting-answer', function (req, res) {
+
+  var requestType = req.session.data['requestType']
+
+  if (requestType == "new-account") {
+
+    res.redirect('/support-app/which-apps-access-to')
+
+  } else if (requestType == "completed-training") {
+
+    res.redirect('/support-app/completed-training')
+
+  } else if (requestType == "refresher-training") {
+
+    res.redirect('/support-app/whitehall-publisher-training')
+
+  }
+
+})
+
+router.post('/support-app/check-apps', function (req, res) {
+
+  var apps = req.session.data['apps'] || []
+
+  var whitehallSelected = apps.includes('whitehall')
+
+  var specialistAppsSelected =
+    apps.includes('manuals-publisher') ||
+    apps.includes('specialist-publisher') ||
+    apps.includes('travel-advice-publisher')
+
+  if (whitehallSelected) {
+
+    // Whitehall selected
+    res.redirect('/support-app/whitehall-publisher-training')
+
+  } else if (specialistAppsSelected) {
+
+    // No Whitehall, but Manuals, Specialist or Travel Advice selected
+    res.redirect('/support-app/writing-for-gov-uk-training')
+
+  } else {
+
+    // No Whitehall and no specialist apps
+    res.redirect('/support-app/who-needs-a-copy-of-this-request')
+
+  }
+
+})
+
+router.post('/support-app/whitehall-publisher-training-answer', function (req, res) {
+  res.render('support-app/who-needs-a-copy-of-this-request')
+})
+
+router.post('/support-app/writing-for-gov-uk-training-answer', function (req, res) {
+  res.render('support-app/who-needs-a-copy-of-this-request')
+})
+
+router.post('/support-app/who-needs-a-copy-of-this-request-answer', function (req, res) {
+  res.render('support-app/check-answers')
+})
+
+router.post('/support-app/completed-training-answer', function (req, res) {
+  res.render('support-app/who-needs-a-copy-of-this-request')
 })
